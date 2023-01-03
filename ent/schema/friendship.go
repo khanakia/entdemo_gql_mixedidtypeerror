766956f1a767f8a2fb5fec352503b420d401b395
khanakia <khanakia@gmail.com>
2023-01-03 13:44:44 +0530

    init

---
+++ ent/schema/friendship.go
@@ -0,0 +1,29 @@
+package schema
+
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
+
+// Friendship holds the edge schema definition of the Friendship relationship.
+type Friendship struct {
+	ent.Schema
+}
+
+// Fields of the Friendship.
+func (Friendship) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("id"),
+		field.Time("created_at").
+			Default(time.Now),
+		field.String("user_id"),
+		field.String("friend_id"),
+	}
+}
+
+// Edges of the Friendship.
+func (Friendship) Edges() []ent.Edge {
+	return nil
+}
---
+++ ent/schema/user.go
@@ -0,0 +1,35 @@
+package schema
+
+import (
+	"entgo.io/contrib/entgql"
+	"entgo.io/ent"
+	"entgo.io/ent/schema"
+	"entgo.io/ent/schema/field"
+)
+
+// User holds the schema definition for the User entity.
+type User struct {
+	ent.Schema
+}
+
+// Fields of the User.
+func (User) Fields() []ent.Field {
+	return []ent.Field{
+		// field.String("id"),
+		field.String("name").
+			Default("unknown"),
+	}
+}
+
+// Edges of the User.
+func (User) Edges() []ent.Edge {
+	return nil
+}
+
+func (User) Annotations() []schema.Annotation {
+	return []schema.Annotation{
+		entgql.QueryField(),
+		entgql.RelayConnection(),
+		// entgql.Mutations(entgql.MutationCreate(), entgql.MutationUpdate()),
+	}
+}
